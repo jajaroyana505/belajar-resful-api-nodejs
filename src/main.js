66b9c42c2f037e950d35b72ba578afd6dde4ddf9
jajaroyana505 <muhammadjajaroyana3@gmail.com>
2024-12-25 22:06:42 +0700

    finish

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { logger } from "./application/logging.js";
 import { web } from "./application/web.js";
 
-web.listen(300, () => {
-  logger.info("App start");
+web.listen(3000, () => {
+  logger.info("App start at http://localhost:3000");
 });
